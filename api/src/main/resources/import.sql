ff7bf70a3f8c3d634e228125b7bcb6e9365e0613
wallaceferreira149 <wallaceferreira149@gmail.com>
2024-06-20 13:37:36 -0400

    chore: :card_file_box: implement seed database

---
+++ api/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO TB_CATEGORY (name) VALUES ('Books');
+INSERT INTO TB_CATEGORY (name) VALUES ('Fantasy');
+INSERT INTO TB_CATEGORY (name) VALUES ('Eletronics');
+INSERT INTO TB_CATEGORY (name) VALUES ('Computers');
